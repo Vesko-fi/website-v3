f2e9438ad905db75faf44cff450d3e9ec5ae3383
SantoshChapagai <you@example.com>
2025-07-07 19:19:53 +0545

    update the styling

--- src/features/for-people/LocalConnectionSection.tsx
+++ src/features/for-people/LocalConnectionSection.tsx
@@ -9,14 +9,14 @@ const LocalConnectionSection = () => {
   const { t } = useTranslation();
   return (
     <Section>
-      <Container>
+      <Container className='flex flex-col gap-4'>
         <div className='flex flex-col gap-2 text-center'>
           <h2 className='text-4xl font-bold md:text-5xl lg:text-6xl'>
             {t("forPeople.localConnection.heading")}
           </h2>
           <p>{t("forPeople.localConnection.subheading")}</p>
         </div>
-        <div className='mt-5 flex flex-col items-center sm:flex-row'>
+        <div className='mt-5 flex flex-col items-center gap-4 sm:flex-row'>
           <div className='flex flex-1 flex-col gap-2'>
             <p className='text-sm'>{t("forPeople.localConnection.step1.label")}</p>
             <h3 className='text-xl font-bold md:text-2xl lg:text-4xl'>
@@ -28,7 +28,7 @@ const LocalConnectionSection = () => {
             <Image src={Assets.searchAndFind} alt='search and find' />
           </div>
         </div>
-        <div className='flex flex-col items-center sm:flex-row-reverse'>
+        <div className='flex flex-col items-center gap-4 sm:flex-row-reverse'>
           <div className='flex flex-1 flex-col gap-2'>
             <p className='text-sm'>{t("forPeople.localConnection.step2.label")}</p>
             <h3 className='text-xl font-bold md:text-2xl lg:text-4xl'>
@@ -42,7 +42,7 @@ const LocalConnectionSection = () => {
             <Image src={Assets.checkAvailablity} alt='check availability' />
           </div>
         </div>
-        <div className='flex flex-col items-center sm:flex-row'>
+        <div className='flex flex-col items-center gap-4 sm:flex-row'>
           <div className='flex flex-1 flex-col gap-2'>
             <p className='text-sm'>{t("forPeople.localConnection.step3.label")}</p>
             <h3 className='text-xl font-bold md:text-2xl lg:text-4xl'>
