91b383f237dd88365f0fe313bc1fe44bb2579a14
SantoshChapagai <you@example.com>
2025-07-09 23:10:25 +0545

    create a layout for the payment and logistics page

---
+++ src/pages/LogisticPage.tsx
@@ -0,0 +1,11 @@
+import { MainContent } from "@/features/logistics/MainContent";
+
+const LogisticPage = () => {
+  return (
+    <div className='relative -top-18'>
+      <MainContent />
+    </div>
+  );
+};
+
+export default LogisticPage;
---
+++ src/pages/PaymentPage.tsx
@@ -0,0 +1,11 @@
+import { MainContent } from "@/features/payment/MainContent";
+
+const PaymentPage = () => {
+  return (
+    <div className='relative -top-18'>
+      <MainContent />
+    </div>
+  );
+};
+
+export default PaymentPage;
