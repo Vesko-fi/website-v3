e628d48d215203e69f25c9041e355ad5ab13437f
sabin <sashre@utu.fi>
2025-07-15 17:31:23 +0300

    Added env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,18 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_APPS_SCRIPT_URL: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_APP_VERSION: string;
+  readonly VITE_APP_ENV: string;
+  readonly VITE_DEBUG: string;
+  // Add other VITE_ prefixed env variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "*.PNG" {
   const src: string;
   export default src;
