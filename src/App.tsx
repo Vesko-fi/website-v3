7bed02d3e99e326808363f56afc5708067f772a1
SantoshChapagai <you@example.com>
2025-07-15 19:35:40 +0545

    update the emailjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { I18nextProvider } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import { ScrollToTop } from "./shared/components/common/ScrollToTop";
 
@@ -15,6 +16,7 @@ const App: React.FC = () => {
 
   return (
     <I18nextProvider i18n={i18n}>
+      <ToastContainer />
       <BrowserRouter>
         <ScrollToTop />
         <LanguageDetectorGuard>
