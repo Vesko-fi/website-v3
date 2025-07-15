665fb533b2d2d16ddfd34f579cfa4a85259bbe8e
SantoshChapagai <you@example.com>
2025-07-15 14:44:38 +0545

    update the links in footer

--- src/shared/constants/navItems.ts
+++ src/shared/constants/navItems.ts
@@ -2,11 +2,6 @@ import type { NavigationItem } from "@/layouts/navigation.types";
 
 export const NAV_ITEMS: NavigationItem[] = [
   {
-    id: "home",
-    label: "nav.home",
-    routeKey: "home",
-  },
-  {
     id: "offline-vendors",
     label: "nav.offline-vendors",
     routeKey: "offlineVendors",
@@ -16,12 +11,31 @@ export const NAV_ITEMS: NavigationItem[] = [
     label: "nav.online-vendors",
     routeKey: "onlineVendors",
   },
+
+  { id: "people", label: "nav.people", routeKey: "people" },
+  {
+    id: "payment",
+    label: "nav.payment",
+    routeKey: "paymentLogistic",
+  },
+  {
+    id: "logistic",
+    label: "nav.logistic",
+    routeKey: "paymentLogistic",
+  },
+];
+
+export const COMPANY_NAV_ITEMS: NavigationItem[] = [
+  {
+    id: "home",
+    label: "nav.home",
+    routeKey: "home",
+  },
   {
     id: "about",
     label: "nav.about",
     routeKey: "about",
   },
-  { id: "people", label: "nav.people", routeKey: "people" },
   {
     id: "contact",
     label: "nav.contact",
