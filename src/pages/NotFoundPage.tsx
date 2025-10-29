b770bd2218c21bc37f3f597e15a12250d295a4a2
sabin <sashre@utu.fi>
2025-10-29 12:31:29 +0200

    remove console

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -5,8 +5,6 @@ const NotFoundPage = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  console.log(t);
-
   return (
     <div className='flex min-h-screen flex-col items-center justify-center p-4 text-center'>
       <h1 className='mb-4 text-4xl font-bold'>{t("notFound.title")}</h1>
