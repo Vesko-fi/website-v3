018f469d48ad0e746e7a6c15769296f9002fcfb7
sabin <sashre@utu.fi>
2025-11-03 15:47:24 +0200

    Fix build issue

--- src/features/for-people/VeskoIsSection.tsx
+++ src/features/for-people/VeskoIsSection.tsx
@@ -85,15 +85,6 @@ const VeskoIsSection: React.FC = () => {
     },
   };
 
-  const subTextVariants = {
-    hidden: { opacity: 0, y: 40 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.8, ease: "easeOut", delay: 0.4 },
-    },
-  };
-
   // Split the main text for the second section
   const mainText = t("forPeople.veskoIs.secondSection.mainText");
 
